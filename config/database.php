c51f97d03aad987c1dbbe4fac63777dfd4b63e13
xixiwenxuanhesss <2212266755@qq.com>
2026-01-08 10:32:41 +0000

    chore(deploy): bootstrap initial installation

--- config/database.php
+++ config/database.php
@@ -4,9 +4,9 @@ declare (strict_types=1);
 return [
     'driver' => 'mysql',
     'host' => '127.0.0.1',
-    'database' => 'demo',
-    'username' => 'demo',
-    'password' => 'TbfXmL2JTcXYYrWZ',
+    'database' => 'acg_faka',
+    'username' => 'xixiwenxuanhe',
+    'password' => 'changsheng252403',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => 'acg_',
